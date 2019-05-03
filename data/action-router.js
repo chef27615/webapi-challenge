5f0d85d4e396f4049dfd0a3c414bf8c4bc1090a7
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-03 13:43:23 -0400

    working on actions in project

--- data/action-router.js
+++ data/action-router.js
@@ -1,6 +1,40 @@
 const express = require('express');
-const Action = require('./helpers/actionModel');
-
+const Actions = require('./helpers/actionModel');
+const Projects = require('./helpers/projectModel');
 const actionRouter = express.Router();
 
+actionRouter.get('/', (req, res) => {
+    try {
+        const action = Actions.get();
+        action ? res.status(200).json(action) : res.status(404).json({ message: "no project no action"})
+    } catch(err){ res.status(500).json({message: "The actions can not be retrieved"})}
+})
+
+actionRouter.post('/', async (req, res) => {
+    try{
+        const action = await Actions.insert(req.body);
+        const { project_id, description, notes} = req.body;
+        project_id && description && notes ? res.status(200).json(action) : res.status(400).json({ message: "some of the required fields are not filled out, please try again"})
+    } catch(err){ res.status(500).json({message: "The actions can not be retrieved"})}
+})
+
+actionRouter.delete('/:id', async (req, res) => {
+    try {
+        const id =  await Actions.remove(req.params.id);
+        id > 0 ? res.status(200).json({ message: "action deleted"}) : res.status(404).json({ message: " can not find the action by this id"})  
+    } catch(err){ res.status(500).json({message: "The actions can not be retrieved"})}
+})
+
+actionRouter.put('/:id', async (req, res) => {
+   try{
+    const action =  await Actions.update(req.body)
+    const { project_id, description, notes } =req.body;
+    project_id && description && notes ? res.status(200).json(action) : res.status(400).json({ message: "something is not right"})
+   } catch(err){ res.status(500).json({message: "The actions can not be retrieved"})}
+})
+
+
+actionRouter.use((req, res, next) => {
+    res.status(404).json({ message: "looking for some action? Not here"})
+})
 module.exports = actionRouter;
--- data/project-router.js
+++ data/project-router.js
@@ -4,6 +4,50 @@ const Projects = require('./helpers/projectModel');
 
 const projectRouter = express.Router();
 
+projectRouter.get('/', async (req, res) => {
+    try{
+        const projects = await Projects.get();
+        res.status(200).json(projects);
+    } catch(err){ res.status(500).json({ error: err});}
+})
 
+projectRouter.post('/', async (req, res) => {
+    try {
+        const newProject = await Projects.insert(req.body)
+        const {name, description} = req.body;
+        name && description ? res.status(200).json(newProject) : res.status(400),json({
+            message: "name and description are both required for project"
+        })
+    } catch(err){ res.status(500).json({ error: err});}
+})
 
+projectRouter.delete('/:id', async (req, res) => {
+    try {
+        const id = await Projects.remove(req.params.id)
+        id > 0 ? res.status(200).json({ message: "project deleted"}) : res.status(404).json({
+            message: "We can not find project associated with this id"
+        })
+    } catch(err){ res.status(500).json({ error: err});}
+})
+
+projectRouter.put('/:id', async (req, res) => {
+    try{
+        const project = Projects.update(req.params.id, req.body);
+        const {name, description} = req.body;
+        name && description ? res.status(200).json(project) : res.status(404).json({
+            message: "something is not right, try again"
+        })
+    } catch(err){ res.status(500).json({ error: err});}
+})
+
+projectRouter.get('/:id/actions', async (req, res) => {
+    try {
+        const projectId = Projects.getProjectActions(req.params.id);
+        projectId ? res.status(200).json(action) : res.status(404).json({ message: "no project no action"})
+    } catch(err){ res.status(500).json({ error: err});}
+})
+
+projectRouter.use((req, res, next) => {
+    res.status(404).json({ message: "Nice try, but, no"})
+})
 module.exports = projectRouter;
